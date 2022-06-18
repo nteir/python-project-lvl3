25e0d0d683eba75806aaf3aea3c197db990775d8
Natalia Teplynina <nt@mnt9.com>
2022-06-18 09:43:55 +0300

    setup poetry script call

--- page_loader/__init__.py
+++ page_loader/__init__.py
@@ -1,3 +1,4 @@
 from page_loader.page_loader import download
 
+
 __all__ = ('download',)
---
+++ page_loader/scripts/page_loader.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import argparse
+
+
+def main():
+    print('Main running')
+
+
+if __name__ == '__main__':
+    main()
