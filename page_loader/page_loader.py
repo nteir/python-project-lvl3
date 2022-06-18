7c4dff7c620ee259d86df8e438de97b846aeee0d
Natalia Teplynina <nt@mnt9.com>
2022-06-18 13:12:53 +0300

    add html download

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,4 +1,26 @@
+import os
+import re
+from urllib.parse import urlparse
+import urllib.request
 
 
 def download(source_path, dest_path):
-    pass
+    if dest_path is None:
+        dest_path = os.getcwd()
+    dest_name = get_output_path(source_path, dest_path)
+    with urllib.request.urlopen(source_path) as html_file:
+        if os.path.exists(dest_path):
+            with open(dest_name, 'w') as output_html:
+                for line in html_file:
+                    output_html.write(line.decode('utf-8'))
+    return dest_name
+
+
+def get_output_path(source_path, dest_path):
+    url = urlparse(source_path)
+    url_path, _ = os.path.splitext(url.path)
+    url_path = '-'.join(filter(None, url_path.split('/')))
+    dest_name = re.sub('[\W^_]', '-', url.netloc)
+    dest_name += f'-{url_path}.html'
+    dest_name = os.path.join(dest_path, dest_name)
+    return dest_name
--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
+from page_loader.page_loader import download
 import argparse
 
 
 def main():
-    print('Main running')
+    parser = argparse.ArgumentParser(
+        description='Downloads a Web page and saves it locally.'
+    )
+    parser.add_argument(
+        "-o", "--output",
+        default=None,
+        help="destination directory path"
+    )
+    parser.add_argument("source")
+    args = parser.parse_args()
+    print(download(args.source, dest_path=args.output))
 
 
 if __name__ == '__main__':
