16c85754f1e67c1b10e3006a31dd31682aec6202
Natalia Teplynina <nt@mnt9.com>
2022-06-21 11:25:35 +0300

    add tests for http requests

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -9,12 +9,14 @@ def download(source_path, dest_path):
         dest_path = os.getcwd()
     dest_name = get_output_path(source_path, dest_path)
     if os.path.isdir(dest_path):
-        with urllib.request.urlopen(source_path) as html_file:
-            with open(dest_name, 'w') as output_html:
-                for line in html_file:
-                    output_html.write(line.decode('utf-8'))
+        with urllib.request.urlopen(source_path) as response:
+            html = response.read().decode('utf-8')
+        with open(dest_name, 'w') as output_html:
+            output_html.write(html)
     else:
-        raise NotADirectoryError('Destination path must be an existing directory')
+        raise NotADirectoryError(
+            'Destination path must be an existing directory'
+        )
     return dest_name
 
 
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,6 +1,21 @@
 from page_loader.page_loader import download
+import os.path
+from unittest.mock import patch, MagicMock
 
 
-def test_download():
-    download('a', 'b')
-    assert True
+@patch('urllib.request.urlopen')
+def test_request_download(mock_urlopen):
+    source_url = 'http://dkfgjrdirjwsopiej.com/host/fake_test.html'
+    dest_dir = './tests/fixtures/'
+    expected_content = 'sample text'
+    expected_name = os.path.join(dest_dir, 'dkfgjrdirjwsopiej-com-host-fake_test.html')
+    mock = MagicMock()
+    mock.getcode.return_value = 200
+    mock.__enter__.return_value = mock
+    mock.read.return_value = expected_content.encode('utf-8')
+    mock_urlopen.return_value = mock
+    received_name = download(source_url, dest_dir)
+    assert received_name == expected_name
+    with open(expected_name) as result_file:
+        received_content = result_file.read()
+    assert received_content == expected_content
