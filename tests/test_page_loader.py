d3e4738e1daf5764aebe4b47aa5730a6ffc6cae6
Natalia Teplynina <nt@mnt9.com>
2022-06-21 14:21:47 +0300

    edit tests

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,21 +1,31 @@
 from page_loader.page_loader import download
 import os.path
+import tempfile
 from unittest.mock import patch, MagicMock
+import pytest
 
 
 @patch('urllib.request.urlopen')
 def test_request_download(mock_urlopen):
     source_url = 'http://dkfgjrdirjwsopiej.com/host/fake_test.html'
-    dest_dir = './tests/fixtures/'
     expected_content = 'sample text'
-    expected_name = os.path.join(dest_dir, 'dkfgjrdirjwsopiej-com-host-fake_test.html')
     mock = MagicMock()
     mock.getcode.return_value = 200
     mock.__enter__.return_value = mock
     mock.read.return_value = expected_content.encode('utf-8')
     mock_urlopen.return_value = mock
-    received_name = download(source_url, dest_dir)
-    assert received_name == expected_name
-    with open(expected_name) as result_file:
-        received_content = result_file.read()
-    assert received_content == expected_content
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        dest_dir = tmpdirname
+        expected_name = os.path.join(dest_dir, 'dkfgjrdirjwsopiej-com-host-fake_test.html')
+        received_name = download(source_url, dest_dir)
+        assert received_name == expected_name
+        with open(expected_name) as result_file:
+            received_content = result_file.read()
+        assert received_content == expected_content
+
+
+def test_exc_nodir_download():
+    source_url = 'http://dkfgjrdirjwsopiej.com/host/fake_test.html'
+    dest_dir = 'notadir'
+    with pytest.raises(NotADirectoryError):
+        download(source_url, dest_dir)
